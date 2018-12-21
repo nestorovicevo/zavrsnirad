60bac9ca880cafcd082156deb1a8ccda2ad84365
FInestorovicevo <nestorovicevo@gmail.com>
2018-12-21 15:53:57 +0100

    validacija implementirana

--- javaScript/validation.js
+++ javaScript/validation.js
@@ -8,13 +8,13 @@ function validateForm()
     }
   }
 
-///// Sva polja su obavezna, uraditi validaciju. Ukoliko neko polje nije uneto, izbaciti warning message da se popune sva polja, nevezano za to koje polje nije popunjeno. Koristiti bootstrapove css klase .alert i .alert-danger
-
 function validateCreateForm()
 {
     var z = document.forms["create-form"]["author"].value;
     var s = document.forms["create-form"]["title"].value;
     var m = document.forms["create-form"]["body"].value;
+
+
 
     if (z == "" || s == "" || m == "") {
       alert("All fields are required!");
